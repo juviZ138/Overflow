2f80b1d59008a8ac805426fa43386d04509a593f
juviZ138 <quachlydientam@gmail.com>
2026-03-21 19:39:32 +0700

    Add: Question service

--- Overflow.AppHost/AppHost.cs
+++ Overflow.AppHost/AppHost.cs
@@ -1,6 +1,21 @@
+#pragma warning disable ASPIRECERTIFICATES001
 var builder = DistributedApplication.CreateBuilder(args);
 
 var keycloak = builder.AddKeycloak("keycloak", 6001)
+    .WithoutHttpsCertificate() 
+#pragma warning restore ASPIRECERTIFICATES001
     .WithDataVolume("keycloak-data");
+
+var postgres = builder.AddPostgres("postgres", port:5432)
+    .WithDataVolume("postgres-data")
+    .WithPgAdmin();
+
+var questionDb = postgres.AddDatabase("questionDb");
+
+var questionService = builder.AddProject<Projects.QuestionService>
+    ("question-svc")
+    .WithReference(keycloak)
+    .WithReference(questionDb)
+    .WaitFor(keycloak); 
 
 builder.Build().Run();
---
+++ QuestionService/Data/Migrations/20260320143718_FixQuestionTitleLength.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QuestionService.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixQuestionTitleLength : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Questions",
+                type: "character varying(3000)",
+                maxLength: 3000,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(36)",
+                oldMaxLength: 36);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Questions",
+                type: "character varying(36)",
+                maxLength: 36,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(3000)",
+                oldMaxLength: 3000);
+        }
+    }
+}
